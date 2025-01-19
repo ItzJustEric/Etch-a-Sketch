0930a596f98783c262a026c5a2af94aa39453b1f
eameyaw2 <ameyawttttt@outlook.com>
2025-01-19 09:41:39 -0600

    added a color picker

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ const eraseBtn = document.querySelector(".erase");
 const resetBtn = document.querySelector(".rese");
 const gameBoardCells = document.querySelectorAll(".game-board-cell");
 const colorBtn = document.querySelector(".color");
+const colorPicker = document.querySelector("#favcolor");
+
 
 
 colorBtn.addEventListener('click', function() {
     gameBoardCells.forEach(cell => {
         cell.addEventListener('mouseover', function() {
-            cell.style.backgroundColor = 'black';
+            cell.style.backgroundColor = colorPicker.value;
         });
     });
 });
